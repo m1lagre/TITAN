8346656664277f1cf5ce9cd3ba15f8f9cac4d9e6
Gabriel Silva <santana.gsilva@gmail.com>
2025-12-23 12:22:24 -0300

    finalizando calcIMC

--- sprint 6/CalcIMC/script.js
+++ sprint 6/CalcIMC/script.js
@@ -3,17 +3,36 @@ const form = document.getElementById('form');
 form.addEventListener('submit', function(event){
     event.preventDefault();
     
-    const weight = document.getElementById('weight').value
-    const height = document.getElementById('height').value
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
     const bmi = (weight/(height*height)).toFixed(2);
 
     const value = document.getElementById('value');
     let description ='';
-    document.getElementById('infos').classList.remove('hidden')
+
+    value.classList.add('attention')
+
+    document.getElementById('infos').classList.remove('hidden');
 
     if (bmi < 18.5) {
         description = "abaixo do peso"
+    } 
+    else if(bmi >= 18.5 && bmi < 25.5) {
+        description = "ideal";
+        value.classList.remove('attention')
+        value.classList.add('normal');
+    } 
+    else if(bmi >= 25.5 && bmi < 30) {
+        description = "sobrepeso"
     }
+    else if(bmi >= 30 && bmi < 35) {
+        description = "obesidade moderada"
+    }
+    else if(bmi >= 35 && bmi <= 40) {
+        description = "obesidade severa"
+    }
+    else { description = "obesidade morbida"}
 
+    value.textContent = bmi.replace('.', ',');
     document.getElementById('description').textContent = description;
 })
