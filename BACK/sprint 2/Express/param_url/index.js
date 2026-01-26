6aa464ff7306198fa1b58faed081089a7eef0a4b
Gabriel Silva <santana.gsilva@gmail.com>
2026-01-26 14:25:08 -0300

    Criando rotas com Express

--- BACK/sprint 2/Express/param_url/index.js
+++ BACK/sprint 2/Express/param_url/index.js
@@ -3,14 +3,32 @@ const app = express()
 const port = 3000 // variavel ambiente
 
 const path = require("path")
+
+//ler o body
+app.use(express.urlencoded({
+    extended:true,}),
+)
+
+app.use(express.json())
+
+
 const basePath = path.join(__dirname, 'templates')
 
 
-app.get('users/add', (req,res) =>{res.sendFile(`${basePath}/userform.html`)})
+app.get('/users/add', (req,res) =>{res.sendFile(`${basePath}/userform.html`)})
 
 app.post('/users/save', (req,res) => {
+    console.log(req.body)
 
+    const name = req.body.name
+    const age = req.body.age
+
+    console.log(`Nome ${name}, Idade ${age}`)
+
+    res.sendFile(`${basePath}/userform.html`)
 })
+
+
 
 app.get('/users/:id',(req,res) => {
 
